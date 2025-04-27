d96e454c8b9eebb9ed9769a85615e9f6c744320b
JEONG SOEE <jj.soxee@gmail.com>
2025-04-28 04:39:59 +0900

    ✨ feat(frontend): サインアップ画面にaxios通信、バリデーション、成功後ログイン画面遷移を実装
    - axiosを使ってサインアップAPIと接続
    - パスワードとパスワード確認の一致チェック追加
    - サインアップ成功時にログイン画面へ自動遷移
    - エラー時にalertでメッセージ表示

--- frontend/src/views/SignupView.vue
+++ frontend/src/views/SignupView.vue
@@ -1,19 +1,39 @@
 <script setup>
 import { ref } from 'vue'
+import axios from 'axios'
+import { useRouter } from 'vue-router'
 
+const router = useRouter()
 const name = ref('')
 const organizations = ref([])
 const organizationOptions = ['開発部', '営業部', '人事部', 'マーケティング部']
 const email = ref('')
 const password = ref('')
+const password_confirmation = ref('')
 
-const handleSignup = () => {
-    console.log('サインアップ送信!', { 
-        name: name.value, 
-        organization: organizations.value, 
-        email: email.value, 
-        password: password.value 
-    })
+const handleSignup = async () => {
+
+    if (password.value !== password_confirmation.value) {
+        alert('パスワードとパスワード確認が一致していません。')
+        return
+    }
+    try{
+        const response = await axios.post(`${import.meta.env.VITE_API_URL}/signup`, {
+            user: {
+                username: name.value,
+                email: email.value,
+                password: password.value,
+                password_confirmation: password.value,
+                organization_id: organizations.value
+            }
+        })
+        console.log('サインアップ成功!', response.data)
+        alert('登録が成功しました！')
+        router.push('/login')
+    } catch (error) {
+        console.error('サインアップ失敗', error.response?.data)
+        alert('登録に失敗しました。')
+    }
 }
 </script>
 
@@ -24,6 +44,7 @@ const handleSignup = () => {
             <input v-model="name" type="text" placeholder="名前" required/>
             <input v-model="email" type="email" placeholder="メールアドレス" required/>
             <input v-model="password" type="password" placeholder="パスワード" required/>
+            <input v-model="password_confirmation" type="password" placeholder="パスワード確認" required/>
             <div class="signup-organiztion">
                 <div v-for="org in organizationOptions" :key="org">
                     <label>
